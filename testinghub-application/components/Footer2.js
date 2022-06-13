c39a371c48eeec63a57f5c15b29e44a44b7a4e32
Quang Le <leanhquang2803@gmail.com>
2022-06-13 11:52:18 -0500

    Changed from Footer to Footer2

--- testinghub-application/components/Footer2.js
+++ testinghub-application/components/Footer2.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-const Footer = () => {
+export default function Footer2() {
   return (
     <footer section>
       <p className="center topPadding">TestingHub © 2022 </p>
     </footer>
   );
-};
-export default Footer;
+}
--- testinghub-application/pages/testing.js
+++ testinghub-application/pages/testing.js
@@ -1,47 +1,41 @@
 import Center from "../components/Center.js";
 import { useSession, getSession } from "next-auth/react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
-
-
 
 export default function Home() {
-    const { data: session } = useSession();
-    const dev = process.env.NODE_ENV == "development";
-    const url = dev
-      ? "http://localhost:3000"
-      : "https://testing-hub-local.vercel.app";
-    if (session) {
-      fetch(url + "/api/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: session.user.name,
-          email: session.user.email,
-          apiKeys: [],
-        }),
-      });
-      // recordLogin( { name: session.user.name, email: session.user.email, apiKeys: [] } )
-    }
-  
-    return (
-        <div>
-            <Header />
-            <div className="h-screen overflow-hidden">
-          
+  const { data: session } = useSession();
+  const dev = process.env.NODE_ENV == "development";
+  const url = dev
+    ? "http://localhost:3000"
+    : "https://testing-hub-local.vercel.app";
+  if (session) {
+    fetch(url + "/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: session.user.name,
+        email: session.user.email,
+        apiKeys: [],
+      }),
+    });
+    // recordLogin( { name: session.user.name, email: session.user.email, apiKeys: [] } )
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className="h-screen overflow-hidden">
         <main className="flex">
           <Center />
         </main>
-        
       </div>
-        </div>
-      
-    );
-  }
-  
-  /*
+    </div>
+  );
+}
+
+/*
   const recordLogin = async (info) => {
     axios({
       method: 'post',
@@ -50,12 +44,11 @@ export default function Home() {
     })
   }
   */
-  
-  export async function getServerSideProps(context) {
-    return {
-      props: {
-        session: await getSession(context),
-      },
-    };
-  }
-  
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
+}
