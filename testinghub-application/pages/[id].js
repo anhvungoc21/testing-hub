07baa359394c1191f8e702d92eb9d135df05e36b
Quang Le <leanhquang2803@gmail.com>
2022-06-13 11:56:48 -0500

    Changed consolelog to console.log

--- testinghub-application/pages/[id].js
+++ testinghub-application/pages/[id].js
@@ -1,9 +1,7 @@
 import { useRouter } from "next/router";
 
 export default function post(props) {
-    const router = useRouter()
-    consolelog(router, 'routes')
-    return (
-        <h2> post {router.query.id} </h2>
-    )
+  const router = useRouter();
+  console.log(router, "routes");
+  return <h2> post {router.query.id} </h2>;
 }
