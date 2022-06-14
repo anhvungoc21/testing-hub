b284416536c4379bfed14e432afa61b12161390a
Quang Le <leanhquang2803@gmail.com>
2022-06-13 19:30:00 -0500

    Changing format

--- testinghub-application/components/Header.js
+++ testinghub-application/components/Header.js
@@ -1,117 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import {
-
   Container,
-
   Collapse,
-
   Navbar,
-
   NavbarToggler,
-
   NavbarBrand,
-
   Nav,
-
   NavItem,
-
-  NavLink
-
-} from 'reactstrap';
-
-
+  NavLink,
+} from "reactstrap";
 
 const Header = () => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const [sticky, setSticky] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
-
-
   useEffect(() => {
-
-    window.addEventListener('scroll', handleScroll);
-
+    window.addEventListener("scroll", handleScroll);
   });
 
-
-
   const handleScroll = () => {
-
     if (window.scrollY > 90) {
-
       setSticky(true);
-
     } else if (window.scrollY < 90) {
-
       setSticky(false);
-
     }
-
-  }
-
-
+  };
 
   return (
-
-    <div className={`header${sticky ? ' sticky' : ''}`}>
-
+    <div className={`header${sticky ? " sticky" : ""}`}>
       <Navbar light expand="md">
-
         <Container>
-
-         
-
           <NavbarToggler onClick={toggle} />
 
           <Collapse isOpen={isOpen} navbar>
-
             <Nav className="m-auto" navbar>
-
-            <NavbarBrand className="logo" href="/">Testing Hub</NavbarBrand>
+              <NavbarBrand className="logo" href="/">
+                Testing Hub
+              </NavbarBrand>
 
               <NavItem>
-
-                <NavLink className="navLink" href="/">Home</NavLink>
-
+                <NavLink className="navLink" href="/">
+                  Home
+                </NavLink>
               </NavItem>
 
               <NavItem>
-
-                <NavLink className="navLink" href="/agency">For Agencies</NavLink>
-
+                <NavLink className="navLink" href="/agency">
+                  For Agencies
+                </NavLink>
               </NavItem>
 
               <NavItem>
-
-                <NavLink className="navLink" href="/brand">For Brands</NavLink>
-
+                <NavLink className="navLink" href="/brand">
+                  For Brands
+                </NavLink>
               </NavItem>
 
               <NavItem>
-
-                <NavLink className="navLink" href="/testing">Account/Testing</NavLink>
-
+                <NavLink className="navLink" href="/testing">
+                  Account/Testing
+                </NavLink>
               </NavItem>
-
             </Nav>
-
           </Collapse>
-
         </Container>
-
       </Navbar>
-
     </div>
-
   );
-
-}
-
-
+};
 
 export default Header;
