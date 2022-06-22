fe2233595abf6c754d550b9ed26b5e7cfec29a14
Anh Vu <anhvungoc.21@gmail.com>
2022-06-22 16:28:48 -0500

    Update account.js

--- testinghub-application/pages/account.js
+++ testinghub-application/pages/account.js
@@ -77,7 +77,7 @@ export default function Account() {
   }
 
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="h-screen overflow-scroll scrollbar-hide">
       <Header />
       <main className="flex">
         <div className="flex flex-col items-center bg-[#FEFAF3] min-h-screen w-full justify-center">
