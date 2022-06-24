de3d31738946166f8397c73254577d204d880508
Quang Le <leanhquang2803@gmail.com>
2022-06-24 13:57:07 -0500

    Fix dropdown size

--- testinghub-application/components/Dropdown.js
+++ testinghub-application/components/Dropdown.js
@@ -11,21 +11,27 @@ import { signOut } from "next-auth/react";
 export default function Dropdown() {
   return (
     <div>
-      <UncontrolledDropdown className="h-full" nav inNavbar>
+      <UncontrolledDropdown className="" nav inNavbar>
         <DropdownToggle nav caret className="">
           My Account
         </DropdownToggle>
-        <DropdownMenu className="relative h-[15vh] p-0" right>
-          <DropdownItem className="h-1/3 py-0" href="/testing">
+        <DropdownMenu className="h-[18.5vh] p-0" right>
+          <DropdownItem
+            className="h-[6vh] py-0 hover:rounded-sm"
+            href="/testing"
+          >
             My Hub
           </DropdownItem>
-          <DropdownItem className="h=1/3 py-0" href="/account">
+          <DropdownItem
+            className="h-[6vh] py-0 hover:rounded-sm"
+            href="/account"
+          >
             Settings
           </DropdownItem>
-          <DropdownItem className="py-0 m-0" divider />
+          <DropdownItem className="h-0 py-0 m-0 z-0" divider />
           <DropdownItem
-            className="h-1/3 pt-0"
-            onClick={() => signOut({ callbackUrl: "http://localhost:3000/" })}
+            className="h-[6vh] py-0 hover:rounded-sm"
+            onClick={() => signOut({ callbackUrl: "/" })}
           >
             Sign out
           </DropdownItem>
